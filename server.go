0b7190e0ce6ef6b2bf2039941034b54e91664d2e
DavidDexter <dmwangi@savannahinformatics.com>
2020-08-25 16:47:11 +0300

    chore : update origins
    Signed-off-by: DavidDexter <dmwangi@savannahinformatics.com>

--- server.go
+++ server.go
@@ -26,7 +26,8 @@ var allowedOrigins = []string{
 	"http://localhost:5000",
 	"https://api-gateway-test.healthcloud.co.ke",
 	"https://api-gateway-prod.healthcloud.co.ke",
-	"https://profile-lermby4k3q-ew.a.run.app",
+	"https://profile-testing-uyajqt434q-ew.a.run.app",
+	"https://profile-prod-uyajqt434q-ew.a.run.app",
 }
 var allowedHeaders = []string{
 	"Authorization", "Accept", "Accept-Charset", "Accept-Language",
