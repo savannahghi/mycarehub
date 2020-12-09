7910c16e1dd6e621838b83af6cd3f9d036523aa9
Mike Farad <mike18farad@gmail.com>
2020-12-09 23:12:39 +0300

    test: Add FindProfile acceptance test

--- graph/acceptance_test.go
+++ graph/acceptance_test.go
@@ -1552,3 +1552,159 @@ func TestUserProfileQuery(t *testing.T) {
 	}
 
 }
+
+func TestGraphQLFindProfile(t *testing.T) {
+	ctx := base.GetPhoneNumberAuthenticatedContext(t)
+	if ctx == nil {
+		t.Errorf("nil context")
+		return
+	}
+
+	graphQLURL := fmt.Sprintf("%s/%s", baseURL, "graphql")
+	headers, err := base.GetGraphQLHeaders(ctx)
+	if err != nil {
+		t.Errorf("unable to get graphql headers: %v", err)
+		return
+	}
+
+	type args struct {
+		query map[string]interface{}
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "valid query request",
+			args: args{
+				query: map[string]interface{}{
+					"query": `query FindProfile{
+						findProfile{
+							id
+						verifiedIdentifiers
+						isApproved
+						termsAccepted
+						msisdns
+						emails
+						photoBase64
+						photoContentType
+						pushTokens
+						covers{
+								payerName
+						  payerSladeCode
+						  memberNumber
+						  memberName
+						}
+						isTester
+						active
+						dateOfBirth
+						gender
+						patientID
+						name
+						bio
+						language
+						practitionerApproved
+						practitionerTermsOfServiceAccepted
+						canExperiment
+						askAgainToSetIsTester
+						askAgainToSetCanExperiment
+						VerifiedEmails {
+						  email
+						  verified
+						}
+						VerifiedPhones {
+						  msisdn
+						  verified
+						}
+						hasPin
+						hasSupplierAccount
+						hasCustomerAccount
+						practitionerHasServices
+					  }
+					}`,
+				},
+			},
+			wantStatus: 200,
+			wantErr:    false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			body, err := mapToJSONReader(tt.args.query)
+			if err != nil {
+				t.Errorf("unable to get GQL JSON io Reader: %s", err)
+				return
+			}
+
+			r, err := http.NewRequest(
+				http.MethodPost,
+				graphQLURL,
+				body,
+			)
+			if err != nil {
+				t.Errorf("unable to compose request: %s", err)
+				return
+			}
+
+			if r == nil {
+				t.Errorf("nil request")
+				return
+			}
+
+			for k, v := range headers {
+				r.Header.Add(k, v)
+			}
+			client := http.Client{
+				Timeout: time.Second * testHTTPClientTimeout,
+			}
+			resp, err := client.Do(r)
+			if err != nil {
+				t.Errorf("request error: %s", err)
+				return
+			}
+
+			dataResponse, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("can't read request body: %s", err)
+				return
+			}
+			if dataResponse == nil {
+				t.Errorf("nil response data")
+				return
+			}
+
+			data := map[string]interface{}{}
+			err = json.Unmarshal(dataResponse, &data)
+			if err != nil {
+				t.Errorf("bad data returned")
+				return
+			}
+
+			if tt.wantErr {
+				_, ok := data["errors"]
+				if !ok {
+					t.Errorf("expected an error")
+					return
+				}
+			}
+
+			if !tt.wantErr {
+				_, ok := data["errors"]
+				if ok {
+					t.Errorf("error not expected %v", data["errors"])
+					return
+				}
+			}
+
+			if tt.wantStatus != resp.StatusCode {
+				t.Errorf("Bad status reponse returned")
+				return
+			}
+
+		})
+	}
+}
