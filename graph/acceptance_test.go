87303921da00625b67f99a95797d4b7dee462b8c
Salaton <elvisdenis056@gmail.com>
2020-12-09 18:44:16 +0300

    Test: User profile graphql query test

--- graph/acceptance_test.go
+++ graph/acceptance_test.go
@@ -360,7 +360,7 @@ func TestRequestPinReset(t *testing.T) {
 				t.Errorf("request error: %s", err)
 				return
 			}
-	
+
 			if resp == nil && !tt.wantErr {
 				t.Errorf("nil response")
 				return
@@ -1063,6 +1063,7 @@ func TestGraphQLVerifyMSISDNAndPIN(t *testing.T) {
 	type args struct {
 		query map[string]interface{}
 	}
+
 	tests := []struct {
 		name       string
 		args       args
@@ -1344,4 +1345,210 @@ func TestSetLanguagePreference(t *testing.T) {
 
 		})
 	}
+}
+
+func TestUserProfileQuery(t *testing.T) {
+	ctx := base.GetAuthenticatedContext(t)
+
+	if ctx == nil {
+		t.Errorf("nil context")
+		return
+	}
+
+	graphQLURL := fmt.Sprintf("%s/%s", baseURL, "graphql")
+	headers, err := base.GetGraphQLHeaders(ctx)
+	if err != nil {
+		t.Errorf("error in getting headers: %w", err)
+		return
+	}
+
+	type args struct {
+		query map[string]interface{}
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "valid query request",
+			args: args{
+				query: map[string]interface{}{
+					"query": `
+					query userProfile(){
+						userProfile(){
+							id
+							verifiedIdentifiers
+							isApproved
+							termsAccepted
+							msisdns
+							emails
+							photoBase64
+							photoContentType
+							pushTokens
+							covers{
+							payerName
+							payerSladeCode
+							memberNumber
+							memberName
+							}
+							isTester
+							active
+							dateOfBirth
+							gender
+							patientID
+							name
+							bio
+							language
+							practitionerApproved
+							practitionerTermsOfServiceAccepted
+							canExperiment
+							askAgainToSetIsTester
+							askAgainToSetCanExperiment
+							VerifiedEmails{
+							email
+							verified
+							}
+							VerifiedPhones{
+							msisdn
+							verified
+							}
+							hasPin
+							hasSupplierAccount
+							hasCustomerAccount
+							practitionerHasServices
+
+						}
+					}`,
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name: "invalid query request",
+			args: args{
+				query: map[string]interface{}{
+					"query": `
+					query userProfile(){
+						userProfile(){
+							id
+							verifiedIdentifiers
+							isApproved
+							randomQuery
+							testingQuery
+							emails
+							photoBase64
+							photoContentType
+							pushTokens
+							covers{
+							payerName
+							payerSladeCode
+							memberNumber
+							memberName
+							}
+							isTester
+							active
+							dateOfBirth
+							gender
+							patientID
+							name
+							bio
+							language
+							practitionerApproved
+							practitionerTermsOfServiceAccepted
+							canExperiment
+							askAgainToSetIsTester
+							askAgainToSetCanExperiment
+							VerifiedEmails{
+							email
+							verified
+							}
+							VerifiedPhones{
+							msisdn
+							verified
+							}
+							hasPin
+							hasSupplierAccount
+							hasCustomerAccount
+							practitionerHasServices
+
+						}
+					}`,
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			body, err := mapToJSONReader(tt.args.query)
+			if err != nil {
+				t.Errorf("unable to get request JSON io Reader: %s", err)
+				return
+			}
+			r, err := http.NewRequest(
+				http.MethodPost,
+				graphQLURL,
+				body,
+			)
+
+			if err != nil {
+				t.Errorf("can't create new request: %v", err)
+				return
+			}
+
+			if r == nil {
+				t.Errorf("nil request")
+				return
+			}
+
+			for k, v := range headers {
+				r.Header.Add(k, v)
+			}
+			client := http.Client{
+				Timeout: time.Second * testHTTPClientTimeout,
+			}
+			resp, err := client.Do(r)
+			if err != nil {
+				t.Errorf("HTTP error: %v", err)
+				return
+			}
+
+			if !tt.wantErr && resp == nil {
+				t.Errorf("unexpected nil response (did not expect an error)")
+				return
+			}
+
+			if tt.wantErr {
+				return
+			}
+
+			data, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("can't read response body: %v", err)
+				return
+			}
+			if data == nil {
+				t.Errorf("nil response body data")
+				return
+			}
+
+			if tt.wantStatus != resp.StatusCode {
+				t.Errorf("expected status %d, got %d and response %s", tt.wantStatus, resp.StatusCode, string(data))
+				return
+			}
+
+			if !tt.wantErr && resp == nil {
+				t.Errorf("unexpected nil response (did not expect an error)")
+				return
+			}
+		})
+	}
+
 }
