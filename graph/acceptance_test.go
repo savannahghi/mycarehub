4adf0d5a6b0019d64803aa99e1a5e1d0c157c0e1
Salaton <elvisdenis056@gmail.com>
2020-12-10 15:02:50 +0300

    test: supplier profile graphQL query test

--- graph/acceptance_test.go
+++ graph/acceptance_test.go
@@ -2732,3 +2732,199 @@ func TestGraphQLUpdateUserProfile(t *testing.T) {
 		})
 	}
 }
+
+func TestSupplierProfileQuery(t *testing.T) {
+	ctx := base.GetAuthenticatedContext(t)
+
+	if ctx == nil {
+		t.Errorf("nil context")
+		return
+	}
+
+	graphQLURL := fmt.Sprintf("%s/%s", baseURL, "graphql")
+	headers, err := base.GetGraphQLHeaders(ctx)
+	if err != nil {
+		t.Errorf("error in getting headers: %w", err)
+		return
+	}
+
+	type args struct {
+		query map[string]interface{}
+	}
+
+	grapqhQLQueryPayload := `
+	query supplierProfile($uid:String!){
+		supplierProfile(uid:$uid){
+			userProfile {
+				id
+				verifiedIdentifiers
+				isApproved
+				termsAccepted
+				msisdns
+				emails
+				photoBase64
+				photoContentType
+				pushTokens
+				covers {
+				  payerName
+				  payerSladeCode
+				  memberNumber
+				  memberName
+				}
+				isTester
+				active
+				dateOfBirth
+				gender
+				patientID
+				name
+				bio
+				language
+				practitionerApproved
+				practitionerTermsOfServiceAccepted
+				canExperiment
+				askAgainToSetIsTester
+				askAgainToSetCanExperiment
+				VerifiedEmails {
+				  email
+				  verified
+				}
+				VerifiedPhones {
+				  msisdn
+				  verified
+				}
+				hasPin
+				hasSupplierAccount
+				hasCustomerAccount
+				practitionerHasServices
+			  }
+			  supplierId
+			  payablesAccount {
+				id
+				name
+				isActive
+				number
+				tag
+				description
+			  }
+			  supplierKYC {
+				accountType
+				identificationDocType
+				identificationDocNumber
+				identificationDocPhotoBase64
+				identificationDocPhotoContentType
+				license
+				cadre
+				profession
+				kraPIN
+				kraPINDocPhoto
+				businessNumber
+				businessNumberDocPhotoBase64
+				businessNumberDocPhotoContentType
+			  }
+			  active
+
+		}
+	}
+	`
+
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "Valid query request",
+			args: args{
+				query: map[string]interface{}{
+					"query": grapqhQLQueryPayload,
+					"variables": map[string]interface{}{
+						"uid": "e59Ag9JaKNRzmrsqMREQrdNJl5m1",
+					},
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name: "invalid query request - has a non-existent uid",
+			args: args{
+				query: map[string]interface{}{
+					"query": grapqhQLQueryPayload,
+					"variables": map[string]interface{}{
+						"uid": "not a valid uid",
+					},
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			body, err := mapToJSONReader(tt.args.query)
+			if err != nil {
+				t.Errorf("unable to get request JSON io Reader: %s", err)
+				return
+			}
+			r, err := http.NewRequest(
+				http.MethodPost,
+				graphQLURL,
+				body,
+			)
+
+			if err != nil {
+				t.Errorf("can't create new request: %v", err)
+				return
+			}
+
+			if r == nil {
+				t.Errorf("nil request")
+				return
+			}
+
+			for k, v := range headers {
+				r.Header.Add(k, v)
+			}
+			client := http.Client{
+				Timeout: time.Second * testHTTPClientTimeout,
+			}
+			resp, err := client.Do(r)
+			if err != nil {
+				t.Errorf("HTTP error: %v", err)
+				return
+			}
+
+			if !tt.wantErr && resp == nil {
+				t.Errorf("unexpected nil response (did not expect an error)")
+				return
+			}
+
+			if tt.wantErr {
+				return
+			}
+
+			data, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("can't read response body: %v", err)
+				return
+			}
+			if data == nil {
+				t.Errorf("nil response body data")
+				return
+			}
+
+			if tt.wantStatus != resp.StatusCode {
+				t.Errorf("expected status %d, got %d and response %s", tt.wantStatus, resp.StatusCode, string(data))
+				return
+			}
+
+			if !tt.wantErr && resp == nil {
+				t.Errorf("unexpected nil response (did not expect an error)")
+				return
+			}
+		})
+	}
+}
