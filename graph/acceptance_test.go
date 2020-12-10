2df3234b21a05f2c40799736b4ee936400bc9854
Mike Farad <mike18farad@gmail.com>
2020-12-10 10:57:11 +0300

    test: Add acceptance test for conirmEmail mutation

--- graph/acceptance_test.go
+++ graph/acceptance_test.go
@@ -12,6 +12,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
 	"gitlab.slade360emr.com/go/base"
 	"gitlab.slade360emr.com/go/profile/graph/profile"
@@ -2216,6 +2217,180 @@ func TestGraphGQLqueryGetProfile(t *testing.T) {
 				_, ok := data["errors"]
 				if ok {
 					t.Errorf("error not expected got error: %w", err)
+					return
+				}
+			}
+
+			if tt.wantStatus != resp.StatusCode {
+				t.Errorf("Bad status reponse returned")
+				return
+			}
+
+		})
+	}
+}
+
+func TestConfirmEmail(t *testing.T) {
+	ctx := base.GetAuthenticatedContext(t)
+	if ctx == nil {
+		t.Errorf("nil context")
+		return
+	}
+
+	graphQLURL := fmt.Sprintf("%s/%s", baseURL, "graphql")
+	headers, err := base.GetGraphQLHeaders(ctx)
+	if err != nil {
+		t.Errorf("error in getting headers: %w", err)
+		return
+	}
+
+	confirmEmailMutation := `mutation ConfirmEmail($emailInput: String!) {
+		confirmEmail(email: $emailInput){
+		id
+	  verifiedIdentifiers
+	  isApproved
+	  termsAccepted
+	  msisdns
+	  emails
+	  photoBase64
+	  photoContentType
+	  pushTokens
+	  covers{
+		payerName
+		payerSladeCode
+		memberNumber
+		memberName
+	  }
+	  isTester
+	  active
+	  dateOfBirth
+	  gender
+	  patientID
+	  name
+	  bio
+	  language
+	  practitionerApproved
+	  practitionerTermsOfServiceAccepted
+	  canExperiment
+	  askAgainToSetIsTester
+	  askAgainToSetCanExperiment
+	  VerifiedEmails {
+				email
+		verified
+	  }
+	  VerifiedPhones {
+		verified
+		msisdn
+	  }
+	  hasPin
+	  hasSupplierAccount
+	  hasCustomerAccount
+	  practitionerHasServices
+	}
+	}`
+
+	type args struct {
+		query map[string]interface{}
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "valid mutation request",
+			args: args{
+				query: map[string]interface{}{
+					"query": confirmEmailMutation,
+					"variables": map[string]interface{}{
+						"emailInput": fmt.Sprintf("test-%s@healthcloud.co.ke", uuid.New()),
+					},
+				},
+			},
+			wantStatus: 200,
+			wantErr:    false,
+		},
+		{
+			name: "invalid email",
+			args: args{
+				query: map[string]interface{}{
+					"query": confirmEmailMutation,
+					"variables": map[string]interface{}{
+						"emailInput": "not avalid email",
+					},
+				},
+			},
+			wantStatus: 200,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			body, err := mapToJSONReader(tt.args.query)
+			if err != nil {
+				t.Errorf("unable to get GQL JSON io Reader: %s", err)
+				return
+			}
+
+			r, err := http.NewRequest(
+				http.MethodPost,
+				graphQLURL,
+				body,
+			)
+			if err != nil {
+				t.Errorf("unable to compose request: %s", err)
+				return
+			}
+
+			if r == nil {
+				t.Errorf("nil request")
+				return
+			}
+
+			for k, v := range headers {
+				r.Header.Add(k, v)
+			}
+			client := http.Client{
+				Timeout: time.Second * testHTTPClientTimeout,
+			}
+			resp, err := client.Do(r)
+			if err != nil {
+				t.Errorf("request error: %s", err)
+				return
+			}
+
+			dataResponse, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("can't read request body: %s", err)
+				return
+			}
+			if dataResponse == nil {
+				t.Errorf("nil response data")
+				return
+			}
+
+			data := map[string]interface{}{}
+			err = json.Unmarshal(dataResponse, &data)
+			if err != nil {
+				t.Errorf("bad data returned")
+				return
+			}
+
+			if tt.wantErr {
+				_, ok := data["errors"]
+				if !ok {
+					t.Errorf("expected an error")
+					return
+				}
+			}
+
+			if !tt.wantErr {
+				_, ok := data["errors"]
+				if ok {
+					t.Errorf("error not expected")
 					return
 				}
 			}
