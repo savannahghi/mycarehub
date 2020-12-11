443c8cb8a7b545e2870e84b4e839005cdb31b7ef
Maluki <malukimuthusi@yahoo.com>
2020-12-11 16:12:11 +0300

    Test: GraphQL mutation addTester.

--- graph/acceptance_test.go
+++ graph/acceptance_test.go
@@ -4599,3 +4599,137 @@ func TestUpdateCustomerMutation(t *testing.T) {
 	}
 
 }
+
+func TestGraphGQLmutationAddTester(t *testing.T) {
+	ctx := base.GetAuthenticatedContext(t)
+	if ctx == nil {
+		t.Errorf("nil context")
+		return
+	}
+
+	graphQLURL := fmt.Sprintf("%s/%s", baseURL, "graphql")
+	headers, err := base.GetGraphQLHeaders(ctx)
+	if err != nil {
+		t.Errorf("error in getting headers: %w", err)
+		return
+	}
+
+	type args struct {
+		query     map[string]interface{}
+		variables map[string]interface{}
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "valid mutation request",
+			args: args{
+				query: map[string]interface{}{
+					"query": `mutation AddTester($email: String!){
+						addTester(email: $email)
+					  }`,
+				},
+				variables: map[string]interface{}{
+					"email": "be.well@bewell.com",
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name: "invalid: wrong email",
+			args: args{
+				query: map[string]interface{}{
+					"query": `mutation AddTester($email: String!){
+						addTester(email: $email)
+					  }`,
+				},
+				variables: map[string]interface{}{
+					"email": "be.@well@bewell.com",
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.args.query["variables"] = tt.args.variables
+
+			body, err := mapToJSONReader(tt.args.query)
+			if err != nil {
+				t.Errorf("unable to get GQL JSON io Reader: %s", err)
+				return
+			}
+
+			r, err := http.NewRequest(
+				http.MethodPost,
+				graphQLURL,
+				body,
+			)
+			if err != nil {
+				t.Errorf("unable to compose request: %s", err)
+				return
+			}
+
+			if r == nil {
+				t.Errorf("nil request")
+				return
+			}
+
+			for k, v := range headers {
+				r.Header.Add(k, v)
+			}
+			client := http.Client{
+				Timeout: time.Second * testHTTPClientTimeout,
+			}
+			resp, err := client.Do(r)
+			if err != nil {
+				t.Errorf("request error: %s", err)
+				return
+			}
+
+			dataResponse, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("can't read request body: %s", err)
+				return
+			}
+			if dataResponse == nil {
+				t.Errorf("nil response data")
+				return
+			}
+
+			data := map[string]interface{}{}
+			err = json.Unmarshal(dataResponse, &data)
+			if err != nil {
+				t.Errorf("bad data returned")
+				return
+			}
+
+			if tt.wantErr {
+				_, ok := data["errors"]
+				if !ok {
+					t.Errorf("expected an error")
+					return
+				}
+			}
+
+			if !tt.wantErr {
+				_, ok := data["errors"]
+				if ok {
+					t.Errorf("error not expected got error: %w", err)
+					return
+				}
+			}
+			if tt.wantStatus != resp.StatusCode {
+				t.Errorf("Bad status reponse returned")
+				return
+			}
+
+		})
+	}
+}
