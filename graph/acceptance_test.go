85a239526a51e993383b66113e8c29fe9f0382e7
Salaton <elvisdenis056@gmail.com>
2020-12-11 17:36:23 +0300

    test: request pin reset graphql query test

--- graph/acceptance_test.go
+++ graph/acceptance_test.go
@@ -4893,3 +4893,152 @@ func TestRecordPostVisitSurveyMutation(t *testing.T) {
 		})
 	}
 }
+
+func TestRequestPinResetQuery(t *testing.T) {
+
+	ctx := base.GetAuthenticatedContext(t)
+	if ctx == nil {
+		t.Errorf("nil context")
+		return
+	}
+
+	var s *profile.Service = profile.NewService()
+
+	set, err := s.SetUserPIN(ctx, base.TestUserPhoneNumberWithPin, base.TestUserPin)
+	if !set {
+		t.Errorf("can't set a test pin")
+	}
+	if err != nil {
+		t.Errorf("can't set a test pin: %v", err)
+		return
+	}
+
+	graphQLURL := fmt.Sprintf("%s/%s", baseURL, "graphql")
+	headers, err := base.GetGraphQLHeaders(ctx)
+	if err != nil {
+		t.Errorf("error in getting headers: %w", err)
+		return
+	}
+
+	graphQLQueryPayload := `query requestPinReset($msisdn: String!){
+		requestPinReset(msisdn: $msisdn)
+	}`
+
+	type args struct {
+		query map[string]interface{}
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "valid request",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphQLQueryPayload,
+					"variables": map[string]interface{}{
+						"msisdn": base.TestUserPhoneNumberWithPin,
+					},
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name: "invalid request",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphQLQueryPayload,
+					"variables": map[string]interface{}{
+						"msisdn": "+",
+					},
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			body, err := mapToJSONReader(tt.args.query)
+
+			if err != nil {
+				t.Errorf("unable to get GQL JSON io Reader: %s", err)
+				return
+			}
+
+			r, err := http.NewRequest(
+				http.MethodPost,
+				graphQLURL,
+				body,
+			)
+
+			if err != nil {
+				t.Errorf("unable to compose request: %s", err)
+				return
+			}
+
+			if r == nil {
+				t.Errorf("nil request")
+				return
+			}
+
+			for k, v := range headers {
+				r.Header.Add(k, v)
+			}
+			client := http.Client{
+				Timeout: time.Second * testHTTPClientTimeout,
+			}
+			resp, err := client.Do(r)
+			if err != nil {
+				t.Errorf("request error: %s", err)
+				return
+			}
+
+			dataResponse, err := ioutil.ReadAll(resp.Body)
+			log.Printf(string(dataResponse))
+			if err != nil {
+				t.Errorf("can't read request body: %s", err)
+				return
+			}
+			if dataResponse == nil {
+				t.Errorf("nil response data")
+				return
+			}
+
+			data := map[string]interface{}{}
+			err = json.Unmarshal(dataResponse, &data)
+			if err != nil {
+				t.Errorf("bad data returned")
+				return
+			}
+
+			if tt.wantErr {
+				_, ok := data["errors"]
+				if !ok {
+					t.Errorf("expected an error")
+					return
+				}
+			}
+
+			if !tt.wantErr {
+				_, ok := data["errors"]
+				if ok {
+					t.Errorf("error not expected")
+					return
+				}
+			}
+
+			if tt.wantStatus != resp.StatusCode {
+				t.Errorf("Bad status reponse returned")
+				return
+			}
+
+		})
+	}
+
+}
