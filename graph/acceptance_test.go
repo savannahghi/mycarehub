aecb92e82680abddc67c4056b2c269296327537a
Salaton <elvisdenis056@gmail.com>
2020-12-11 16:39:47 +0300

    test: record post visit survey graphql mutation test

--- graph/acceptance_test.go
+++ graph/acceptance_test.go
@@ -1381,7 +1381,7 @@ func TestVerifyEmailOTPMutation(t *testing.T) {
 			}
 
 			dataResponse, err := ioutil.ReadAll(resp.Body)
-			log.Printf(string(dataResponse))
+
 			if err != nil {
 				t.Errorf("can't read request body: %s", err)
 				return
@@ -4659,6 +4659,167 @@ func TestGraphGQLmutationAddTester(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.args.query["variables"] = tt.args.variables
+
+			body, err := mapToJSONReader(tt.args.query)
+			if err != nil {
+				t.Errorf("unable to get GQL JSON io Reader: %s", err)
+				return
+			}
+
+			r, err := http.NewRequest(
+				http.MethodPost,
+				graphQLURL,
+				body,
+			)
+			if err != nil {
+				t.Errorf("unable to compose request: %s", err)
+				return
+			}
+
+			if r == nil {
+				t.Errorf("nil request")
+				return
+			}
+
+			for k, v := range headers {
+				r.Header.Add(k, v)
+			}
+			client := http.Client{
+				Timeout: time.Second * testHTTPClientTimeout,
+			}
+			resp, err := client.Do(r)
+			if err != nil {
+				t.Errorf("request error: %s", err)
+				return
+			}
+
+			dataResponse, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("can't read request body: %s", err)
+				return
+			}
+			if dataResponse == nil {
+				t.Errorf("nil response data")
+				return
+			}
+
+			data := map[string]interface{}{}
+			err = json.Unmarshal(dataResponse, &data)
+			if err != nil {
+				t.Errorf("bad data returned")
+				return
+			}
+
+			if tt.wantErr {
+				_, ok := data["errors"]
+				if !ok {
+					t.Errorf("expected an error")
+					return
+				}
+			}
+
+			if !tt.wantErr {
+				_, ok := data["errors"]
+				if ok {
+					t.Errorf("error not expected got error: %w", err)
+					return
+				}
+			}
+			if tt.wantStatus != resp.StatusCode {
+				t.Errorf("Bad status reponse returned")
+				return
+			}
+
+		})
+	}
+}
+
+func TestRecordPostVisitSurveyMutation(t *testing.T) {
+	ctx := base.GetAuthenticatedContext(t)
+
+	if ctx == nil {
+		t.Errorf("nil context")
+		return
+	}
+
+	graphQLURL := fmt.Sprintf("%s/%s", baseURL, "graphql")
+	headers, err := base.GetGraphQLHeaders(ctx)
+	if err != nil {
+		t.Errorf("error in getting headers: %w", err)
+		return
+	}
+
+	graphQLMutationPayload := `
+	mutation recordPostVisitSurvey($input:PostVisitSurveyInput!){
+		recordPostVisitSurvey(input:$input)
+	}
+	`
+
+	type args struct {
+		query map[string]interface{}
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "valid mutation request",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphQLMutationPayload,
+					"variables": map[string]interface{}{
+						"input": map[string]interface{}{
+							"likelyToRecommend": 9,
+							"criticism":         "Nothing",
+							"suggestions":       "Everything is perfect",
+						},
+					},
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name: "invalid mutation request - has wrong variable types",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphQLMutationPayload,
+					"variables": map[string]interface{}{
+						"input": map[string]interface{}{
+							"likelyToRecommend": "8",
+							"criticism":         1200,
+							"suggestions":       "Everything is perfect",
+						},
+					},
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    true,
+		},
+		{
+			name: "invalid mutation request - has empty values",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphQLMutationPayload,
+					"variables": map[string]interface{}{
+						"input": map[string]interface{}{
+							"likelyToRecommend": "",
+							"criticism":         "",
+							"suggestions":       "",
+						},
+					},
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 
 			body, err := mapToJSONReader(tt.args.query)
 			if err != nil {
