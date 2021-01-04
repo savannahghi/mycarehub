062a9d1d1fa1c279064e8fbec06c6d9c80075ff8
Charles Muchogo <charles@healthcloud.co.ke>
2021-01-04 15:26:33 +0300

    test: request pin reset endpoint

--- pkg/onboarding/usecases/user_pin_test.go
+++ pkg/onboarding/usecases/user_pin_test.go
@@ -214,3 +214,97 @@ func TestUserPinUseCaseImpl_ChangeUserPIN(t *testing.T) {
 		})
 	}
 }
+
+func TestUserPinUseCaseImpl_RequestPINReset(t *testing.T) {
+	ctx := context.Background()
+	flavour := base.FlavourConsumer
+	s, err := InitializeTestService(ctx)
+	if err != nil {
+		t.Errorf("unable to initialize test service")
+		return
+	}
+
+	u, err := createTestUserByPhone(
+		ctx,
+		flavour,
+		base.TestUserPhoneNumber,
+		base.TestUserPin,
+	)
+	if err != nil {
+		t.Errorf("failed to create test phone user: %v", err)
+		return
+	}
+	if !u {
+		t.Errorf("failed to create a test user")
+		return
+	}
+
+	type args struct {
+		ctx   context.Context
+		phone string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "happy case: valid pin reset request",
+			args: args{
+				ctx:   ctx,
+				phone: base.TestUserPhoneNumber,
+			},
+			wantErr: false,
+		},
+		{
+			name: "sad case: invalid pin reset request - empty phone number",
+			args: args{
+				ctx:   ctx,
+				phone: "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "sad case: invalid pin reset request - wrong phone number",
+			args: args{
+				ctx:   ctx,
+				phone: base.TestUserPhoneNumberWithPin, // Not the same with primary
+				// phone number (TestUserPhoneNumber)
+			},
+			wantErr: true,
+		},
+		{
+			name: "sad case: incorrect phone number format",
+			args: args{
+				ctx:   ctx,
+				phone: "+2541234",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pin := s
+			otpResponse, err := pin.UserPIN.RequestPINReset(tt.args.ctx, tt.args.phone)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UserPinUseCaseImpl.RequestPINReset() error = %v, wantErr %v",
+					err,
+					tt.wantErr,
+				)
+				return
+			}
+			if tt.wantErr && otpResponse != "" {
+				t.Errorf("expected nil auth response but got %v, since the error %v occurred",
+					otpResponse,
+					err,
+				)
+				return
+			}
+
+			if !tt.wantErr && otpResponse == "" {
+				t.Errorf("expected an auth response but got nil, since no error occurred")
+				return
+			}
+		})
+	}
+}
