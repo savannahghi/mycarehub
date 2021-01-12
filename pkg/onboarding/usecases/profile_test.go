c9b831c33eed07646e56c156651c8af19c8d2f97
Mike Farad <mike18farad@gmail.com>
2021-01-12 13:02:22 +0300

    test: Add UserProfile Integration Test

--- pkg/onboarding/usecases/profile_test.go
+++ pkg/onboarding/usecases/profile_test.go
@@ -2,7 +2,10 @@ package usecases_test
 
 import (
 	"context"
+
 	"testing"
+
+	"gitlab.slade360emr.com/go/base"
 )
 
 func TestMaskPhoneNumbers(t *testing.T) {
@@ -118,6 +121,110 @@ func TestProfileUseCaseImpl_GetUserProfileByUID(t *testing.T) {
 				return
 			}
 
+		})
+	}
+}
+
+func TestProfileUseCaseImpl_UserProfile(t *testing.T) {
+	ctx, _, err := GetTestAuthenticatedContext(t)
+	if err != nil {
+		t.Errorf("could not get test authenticated context")
+		return
+	}
+	s, err := InitializeTestService(ctx)
+	if err != nil {
+		t.Errorf("unable to initialize test service")
+		return
+	}
+
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *base.UserProfile
+		wantErr bool
+	}{
+		{
+			name: "valid: user profile retrieved",
+			args: args{
+				ctx: ctx,
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid: unauthenticated context supplied",
+			args: args{
+				ctx: context.Background(),
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := s.Onboarding.UserProfile(tt.args.ctx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ProfileUseCaseImpl.UserProfile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if (got == nil) != tt.wantErr {
+				t.Errorf("nil user profile returned")
+				return
+			}
+		})
+	}
+}
+
+func TestProfileUseCaseImpl_GetProfileByID(t *testing.T) {
+
+	ctx, _, err := GetTestAuthenticatedContext(t)
+	if err != nil {
+		t.Errorf("could not get test authenticated context")
+		return
+	}
+
+	s, err := InitializeTestService(ctx)
+	if err != nil {
+		t.Errorf("unable to initialize test service")
+		return
+	}
+
+	profile, err := s.Onboarding.UserProfile(ctx)
+	if err != nil {
+		t.Errorf("could not retreive user profile")
+		return
+	}
+
+	type args struct {
+		ctx context.Context
+		id  *string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "valid: user profile retreived",
+			args: args{
+				ctx: ctx,
+				id:  &profile.ID,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := s.Onboarding.GetProfileByID(tt.args.ctx, tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ProfileUseCaseImpl.GetProfileByID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if (got == nil) != tt.wantErr {
+				t.Errorf("nil user profile returned")
+				return
+			}
 		})
 	}
 }
