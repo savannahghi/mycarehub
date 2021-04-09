03009a364747cabcf2ea817b809605495e3da6d4
Calvine Otieno <calvine.otieno@healthcloud.co.ke>
2021-04-09 12:28:46 +0300

    chore: reduce the number of firebase reads for integration tests
    Use table driven tests

--- pkg/onboarding/usecases/supplier_integration_test.go
+++ pkg/onboarding/usecases/supplier_integration_test.go
@@ -1811,73 +1811,87 @@ func TestFindSupplierByID(t *testing.T) {
 	assert.NotNil(t, spr)
 }
 
-func TestSupplierEDILogin(t *testing.T) {
+func TestSupplierEDIUserLogin(t *testing.T) {
 	s, err := InitializeTestService(context.Background())
 	if err != nil {
-		t.Error("failed to setup signup usecase")
-	}
-
-	primaryPhone := base.TestUserPhoneNumber
-
-	// clean up
-	_ = s.Signup.RemoveUserByPhoneNumber(context.Background(), primaryPhone)
-
-	otp, err := generateTestOTP(t, primaryPhone)
-	if err != nil {
-		t.Errorf("failed to generate test OTP: %v", err)
+		t.Errorf("unable to initialize test service")
 		return
 	}
-	pin := "1234"
-	resp1, err := s.Signup.CreateUserByPhone(
-		context.Background(),
-		&resources.SignUpInput{
-			PhoneNumber: &primaryPhone,
-			PIN:         &pin,
-			Flavour:     base.FlavourPro,
-			OTP:         &otp.OTP,
-		},
-	)
-	assert.Nil(t, err)
-	assert.NotNil(t, resp1)
-	assert.NotNil(t, resp1.Profile)
-	assert.NotNil(t, resp1.CustomerProfile)
-	assert.NotNil(t, resp1.SupplierProfile)
-
-	login1, err := s.Login.LoginByPhone(context.Background(), primaryPhone, pin, base.FlavourPro)
-	assert.Nil(t, err)
-	assert.NotNil(t, login1)
-	assert.NotNil(t, login1.SupplierProfile)
-	assert.Equal(t, resp1.SupplierProfile.ID, login1.SupplierProfile.ID)
-	assert.Equal(t, resp1.SupplierProfile.ProfileID, login1.SupplierProfile.ProfileID)
-
-	// create authenticated context
-	ctx := context.Background()
-	authCred := &auth.Token{UID: login1.Auth.UID}
-	authenticatedContext := context.WithValue(
-		ctx,
-		base.AuthTokenContextKey,
-		authCred,
-	)
-	s, _ = InitializeTestService(authenticatedContext)
+	ctx, _, err := GetTestAuthenticatedContext(t)
+	if err != nil {
+		t.Errorf("failed to get test authenticated context: %v", err)
+		return
+	}
 
 	name := "Makmende And Sons"
 	partnerPractitioner := base.PartnerTypePractitioner
-	resp2, err := s.Supplier.AddPartnerType(authenticatedContext, &name, &partnerPractitioner)
+
+	// TestEDIPortalUsername is a test username for `test` EDI Login
+	TestEDIPortalUsername := "malibu.pharmacy-3873@healthcloud.co.ke"
+
+	// TestEDIPortalPassword is a test password for `test` EDI Login
+	TestEDIPortalPassword := "test provider one"
+
+	WrongTestEDIPortalUsername := "username"
+	WrongTestEDIPortalPassword := "password"
+	EmptyWrongTestEDIPortalUsername := ""
+	EmptyTestEDIPortalPassword := ""
+	resp2, err := s.Supplier.AddPartnerType(ctx, &name, &partnerPractitioner)
 	assert.Nil(t, err)
 	assert.NotNil(t, resp2)
 	assert.Equal(t, true, resp2)
 
-	resp3, err := s.Supplier.SetUpSupplier(authenticatedContext, base.AccountTypeOrganisation)
+	resp3, err := s.Supplier.SetUpSupplier(ctx, base.AccountTypeOrganisation)
 	assert.Nil(t, err)
 	assert.NotNil(t, resp3)
 	assert.Equal(t, false, resp3.Active)
 	assert.Nil(t, resp3.EDIUserProfile)
 
-	resp4, err := s.Supplier.SupplierEDILogin(authenticatedContext, TestEDIPortalUsername, TestEDIPortalPassword, TestSladeCode)
-	assert.Nil(t, err)
-	assert.NotNil(t, resp4)
-	assert.NotNil(t, resp4.Supplier)
-	assert.NotNil(t, resp4.Branches)
+	type args struct {
+		username *string
+		password *string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *base.EDIUserProfile
+		wantErr bool
+	}{
+		{
+			name: "Happy Case: valid credentials",
+			args: args{
+				username: &TestEDIPortalUsername,
+				password: &TestEDIPortalPassword,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Sad Case: Wrong username and password",
+			args: args{
+				username: &WrongTestEDIPortalUsername,
+				password: &WrongTestEDIPortalPassword,
+			},
+			wantErr: true,
+		},
+		{
+			name: "sad case: empty username and password",
+			args: args{
+				username: &EmptyWrongTestEDIPortalUsername,
+				password: &EmptyTestEDIPortalPassword,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := s.Supplier.EDIUserLogin(tt.args.username, tt.args.password)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SupplierUseCasesImpl.EDIUserLogin() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+		})
+	}
 }
 
 func TestFetchSupplierAllowedLocations(t *testing.T) {
@@ -1958,139 +1972,64 @@ func TestFetchSupplierAllowedLocations(t *testing.T) {
 }
 
 func TestSuspendSupplier(t *testing.T) {
-
-	s, err := InitializeTestService(context.Background())
+	ctx, _, err := GetTestAuthenticatedContext(t)
 	if err != nil {
-		t.Error("failed to setup signup usecase")
-	}
-
-	primaryPhone := base.TestUserPhoneNumber
-
-	// clean up
-	_ = s.Signup.RemoveUserByPhoneNumber(context.Background(), primaryPhone)
-
-	otp, err := generateTestOTP(t, primaryPhone)
-	if err != nil {
-		t.Errorf("failed to generate test OTP: %v", err)
+		t.Errorf("failed to get test authenticated context: %v", err)
 		return
 	}
-	pin := "1234"
-	resp1, err := s.Signup.CreateUserByPhone(
-		context.Background(),
-		&resources.SignUpInput{
-			PhoneNumber: &primaryPhone,
-			PIN:         &pin,
-			Flavour:     base.FlavourPro,
-			OTP:         &otp.OTP,
-		},
-	)
-	assert.Nil(t, err)
-	assert.NotNil(t, resp1)
-	assert.NotNil(t, resp1.Profile)
-	assert.NotNil(t, resp1.CustomerProfile)
-	assert.NotNil(t, resp1.SupplierProfile)
 
-	login1, err := s.Login.LoginByPhone(context.Background(), primaryPhone, pin, base.FlavourPro)
-	assert.Nil(t, err)
-	assert.NotNil(t, login1)
-	assert.NotNil(t, login1.SupplierProfile)
-	assert.Equal(t, resp1.SupplierProfile.ID, login1.SupplierProfile.ID)
-	assert.Equal(t, resp1.SupplierProfile.ProfileID, login1.SupplierProfile.ProfileID)
-	assert.Equal(t, resp1.SupplierProfile.ProfileID, login1.SupplierProfile.ProfileID)
-
-	// create authenticated context
-	ctx := context.Background()
-	authCred := &auth.Token{UID: login1.Auth.UID}
-	authenticatedContext := context.WithValue(
-		ctx,
-		base.AuthTokenContextKey,
-		authCred,
-	)
-	s, _ = InitializeTestService(authenticatedContext)
-	// fetch the profile and assert  the permissions slice is empty
-	pr, err := s.Onboarding.UserProfile(authenticatedContext)
-	assert.Nil(t, err)
-	assert.NotNil(t, pr)
-	assert.Equal(t, 0, len(pr.Permissions))
-
-	// now update the permissions
-	perms := []base.PermissionType{base.PermissionTypeAdmin}
-	err = s.Onboarding.UpdatePermissions(authenticatedContext, perms)
-	assert.Nil(t, err)
-
-	// fetch the profile and assert  the permissions slice is not empty
-	pr, err = s.Onboarding.UserProfile(authenticatedContext)
-	assert.Nil(t, err)
-	assert.NotNil(t, pr)
-	assert.Equal(t, 1, len(pr.Permissions))
+	s, err := InitializeTestService(ctx)
+	if err != nil {
+		t.Errorf("unable to initialize test service")
+		return
+	}
 
 	name := "Makmende And Sons"
 	partnerPractitioner := base.PartnerTypePractitioner
-	resp2, err := s.Supplier.AddPartnerType(authenticatedContext, &name, &partnerPractitioner)
+
+	// Add PartnerType
+	resp2, err := s.Supplier.AddPartnerType(ctx, &name, &partnerPractitioner)
 	assert.Nil(t, err)
 	assert.NotNil(t, resp2)
 	assert.Equal(t, true, resp2)
-
-	resp3, err := s.Supplier.SetUpSupplier(authenticatedContext, base.AccountTypeOrganisation)
-	assert.Nil(t, err)
-	assert.NotNil(t, resp3)
-	assert.Equal(t, false, resp3.Active)
-	assert.Nil(t, resp3.EDIUserProfile)
-
-	validInput := domain.IndividualRider{
-		IdentificationDoc: domain.Identification{
-			IdentificationDocType:           domain.IdentificationDocTypeNationalid,
-			IdentificationDocNumber:         "123456789",
-			IdentificationDocNumberUploadID: "id-upload",
-		},
-		KRAPIN:                         "someKRAPIN",
-		KRAPINUploadID:                 "KRAPINUploadID",
-		DrivingLicenseID:               "license",
-		CertificateGoodConductUploadID: "upload1",
-		SupportingDocuments: []domain.SupportingDocument{
-			{
-				SupportingDocumentTitle:       "support-title",
-				SupportingDocumentDescription: "support-description",
-				SupportingDocumentUpload:      "support-upload-id",
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "sad case: suspend a nonexisting supplier",
+			args: args{
+				ctx: context.Background(),
 			},
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name: "Happy case: suspend an existing supplier",
+			args: args{
+				ctx: ctx,
+			},
+			want:    true,
+			wantErr: false,
 		},
 	}
-
-	// submit first kyc. this should pass
-	kyc1, err := s.Supplier.AddIndividualRiderKyc(authenticatedContext, validInput)
-	assert.Nil(t, err)
-	assert.NotNil(t, kyc1)
-
-	// now fetch kyc processing requests
-	kycrequests, err := s.Supplier.FetchKYCProcessingRequests(authenticatedContext)
-	assert.Nil(t, err)
-	assert.Equal(t, 1, len(kycrequests))
-
-	firstKYC := kycrequests[0]
-	assert.Equal(t, false, firstKYC.Processed)
-
-	response, err := s.Supplier.ProcessKYCRequest(authenticatedContext, firstKYC.ID, domain.KYCProcessStatusApproved, nil)
-	assert.Nil(t, err)
-	assert.Equal(t, true, response)
-
-	// fetch the supplier profile. active should be true now
-	sup, err := s.Supplier.FindSupplierByUID(authenticatedContext)
-	assert.Nil(t, err)
-	assert.NotNil(t, sup)
-	assert.Equal(t, true, sup.Active)
-
-	// now suspend the supplier
-	v, err := s.Supplier.SuspendSupplier(authenticatedContext)
-	assert.Nil(t, err)
-	assert.NotNil(t, v)
-	assert.Equal(t, true, v)
-
-	// fetch the supplier profile. active should be false now
-	sup, err = s.Supplier.FindSupplierByUID(authenticatedContext)
-	assert.Nil(t, err)
-	assert.NotNil(t, sup)
-	assert.Equal(t, false, sup.Active)
-
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := s.Supplier.SuspendSupplier(tt.args.ctx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SupplierUseCasesImpl.SuspendSupplier() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("SupplierUseCasesImpl.SuspendSupplier() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
 // TODO: improve test by adding good testcases and properly asserting responses
