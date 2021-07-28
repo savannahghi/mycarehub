e660a2477c178c07eb8fc9a7f0e0a3d31938e88d
Otieno Calvine <35563516+NYARAS@users.noreply.github.com>
2021-07-28 10:34:26 +0300

    feat: add CodeQL analysis workflow (#10)
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

--- .github/scripts/decrypt_secret.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $GITHUB_WORKSPACE/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --verbose --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output $GITHUB_WORKSPACE/secrets/gcs.json gcs.json.gpg
