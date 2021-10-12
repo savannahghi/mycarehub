4451606da3433456da692b161da5fc672c1d251f
Otieno Calvine <35563516+NYARAS@users.noreply.github.com>
2021-10-12 13:43:05 +0300

    chore: wire up ci configuration and unit tests (#22)
    Depend on postgreSQL docker image for integration tests
    Write mocks for unit testing
    
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

--- pkg/onboarding/infrastructure/database/postgres/pg_create.go
+++ pkg/onboarding/infrastructure/database/postgres/pg_create.go
@@ -12,7 +12,13 @@ import (
 // CreateFacility is responsible from creating a representation of a facility
 // A facility here is the healthcare facility that are on the platform.
 // A facility MFL CODE must be unique across the platform. I forms part of the unique identifiers
+//
+// TODO: Create a helper the checks for all required fields
+// TODO: Make the create method idempotent
 func (d *OnboardingDb) CreateFacility(ctx context.Context, facility *dto.FacilityInput) (*domain.Facility, error) {
+	if facility.Code == "" {
+		return nil, fmt.Errorf("`code` should be defined")
+	}
 
 	facilityObj := &gorm.Facility{
 		Name:        facility.Name,
