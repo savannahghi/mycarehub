f614315540c187f18b4118be82724ae66181b4c7
Otieno Calvine <nyarangaotieno@gmail.com>
2021-10-12 12:57:13 +0300

    chore: wire up ci configuration and unit tests
    Depend on postgreSQL docker image for integration tests
    Write mocks for unit testing
    
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

---
+++ pkg/onboarding/infrastructure/database/postgres/gorm/mock/gorm_mock.go
@@ -0,0 +1,62 @@
+package mock
+
+import (
+	"context"
+
+	"github.com/savannahghi/onboarding-service/pkg/onboarding/infrastructure/database/postgres/gorm"
+)
+
+// GormMock struct implements mocks of `gorm's`internal methods.
+//
+// This mock struct should be separate from our own internal methods.
+type GormMock struct {
+	CreateFacilityFn func(ctx context.Context, facility *gorm.Facility) (*gorm.Facility, error)
+	RetrieveFn       func(id *int64) (*gorm.Facility, error)
+}
+
+// NewGormMock initializes a new instance of `GormMock` then mocking the case of success.
+func NewGormMock() *GormMock {
+	return &GormMock{
+		CreateFacilityFn: func(ctx context.Context, facility *gorm.Facility) (*gorm.Facility, error) {
+			id := int64(1)
+			name := "Kanairo One"
+			code := "KN001"
+			county := "Kanairo"
+			description := "This is just for mocking"
+			return &gorm.Facility{
+				FacilityID:  &id,
+				Name:        name,
+				Code:        code,
+				Active:      true,
+				County:      county,
+				Description: description,
+			}, nil
+		},
+
+		RetrieveFn: func(id *int64) (*gorm.Facility, error) {
+			facilityID := int64(1)
+			name := "Kanairo One"
+			code := "KN001"
+			county := "Kanairo"
+			description := "This is just for mocking"
+			return &gorm.Facility{
+				FacilityID:  &facilityID,
+				Name:        name,
+				Code:        code,
+				Active:      true,
+				County:      county,
+				Description: description,
+			}, nil
+		},
+	}
+}
+
+// CreateFacility mocks the implementation of `gorm's` CreateFacility method.
+func (gm *GormMock) CreateFacility(ctx context.Context, facility *gorm.Facility) (*gorm.Facility, error) {
+	return gm.CreateFacilityFn(ctx, facility)
+}
+
+// Retrieve mocks the implementation of `gorm's` Retrieve method.
+func (gm *GormMock) Retrieve(id *int64) (*gorm.Facility, error) {
+	return gm.RetrieveFn(id)
+}
---
+++ pkg/onboarding/infrastructure/database/postgres/mock/pg_mock.go
@@ -0,0 +1,39 @@
+package mock
+
+import (
+	"context"
+
+	"github.com/savannahghi/onboarding-service/pkg/onboarding/application/dto"
+	"github.com/savannahghi/onboarding-service/pkg/onboarding/domain"
+)
+
+// PostgresMock struct implements mocks of `postgres's` internal methods.
+type PostgresMock struct {
+	CreateFacilityFn func(ctx context.Context, facility *dto.FacilityInput) (*domain.Facility, error)
+}
+
+// NewPostgresMock initializes a new instance of `GormMock` then mocking the case of success.
+func NewPostgresMock() *PostgresMock {
+	return &PostgresMock{
+		CreateFacilityFn: func(ctx context.Context, facility *dto.FacilityInput) (*domain.Facility, error) {
+			id := int64(1)
+			name := "Kanairo One"
+			code := "KN001"
+			county := "Kanairo"
+			description := "This is just for mocking"
+			return &domain.Facility{
+				ID:          id,
+				Name:        name,
+				Code:        code,
+				Active:      true,
+				County:      county,
+				Description: description,
+			}, nil
+		},
+	}
+}
+
+// CreateFacility mocks the implementation of `gorm's` CreateFacility method.
+func (gm *PostgresMock) CreateFacility(ctx context.Context, facility *dto.FacilityInput) (*domain.Facility, error) {
+	return gm.CreateFacilityFn(ctx, facility)
+}
