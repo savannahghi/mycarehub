2a6ab85ccd2e2f9a6bbcb4214893e49f5c231bf4
Otieno Calvine <35563516+NYARAS@users.noreply.github.com>
2021-10-15 11:11:17 +0300

    chore: scaffold client and user usecases (#33)
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

--- pkg/onboarding/usecases/usecase.go
+++ pkg/onboarding/usecases/usecase.go
@@ -2,8 +2,10 @@ package usecases
 
 import (
 	"github.com/savannahghi/onboarding-service/pkg/onboarding/infrastructure"
+	"github.com/savannahghi/onboarding-service/pkg/onboarding/usecases/client"
 	"github.com/savannahghi/onboarding-service/pkg/onboarding/usecases/facility"
 	"github.com/savannahghi/onboarding-service/pkg/onboarding/usecases/metric"
+	"github.com/savannahghi/onboarding-service/pkg/onboarding/usecases/user"
 	engagementSvc "github.com/savannahghi/onboarding/pkg/onboarding/infrastructure/services/engagement"
 )
 
@@ -12,6 +14,8 @@ type Interactor struct {
 	*engagementSvc.ServiceEngagementImpl
 	*facility.UseCaseFacilityImpl
 	*metric.UsecaseMetricsImpl
+	*user.UseCasesUserImpl
+	*client.UseCasesClientImpl
 }
 
 // NewUsecasesInteractor initializes a new usecases interactor
@@ -19,10 +23,14 @@ func NewUsecasesInteractor(infrastructure infrastructure.Interactor) Interactor 
 	var engagement *engagementSvc.ServiceEngagementImpl
 	facility := facility.NewFacilityUsecase(infrastructure)
 	metrics := metric.NewMetricUsecase(infrastructure)
+	user := user.NewUseCasesUserImpl(infrastructure)
+	client := client.NewUseCasesClientImpl(infrastructure)
 
 	return Interactor{
 		engagement,
 		facility,
 		metrics,
+		user,
+		client,
 	}
 }
