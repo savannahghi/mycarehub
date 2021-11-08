99a06af14792ec33819cae31dc094962f49c5063
Salaton <nairouasalaton@gmail.com>
2021-11-08 17:25:33 +0300

    test: setup initial acceptance tests (#109)

---
+++ tests/config_test.go
@@ -0,0 +1,162 @@
+package tests
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/imroc/req"
+	"github.com/savannahghi/firebasetools"
+	externalExtension "github.com/savannahghi/mycarehub/pkg/mycarehub/application/extension"
+	"github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/database/postgres"
+	"github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/database/postgres/gorm"
+	"github.com/savannahghi/mycarehub/pkg/mycarehub/presentation"
+	"github.com/savannahghi/mycarehub/pkg/mycarehub/presentation/interactor"
+	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/facility"
+	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/user"
+	"github.com/savannahghi/serverutils"
+)
+
+const (
+	testHTTPClientTimeout = 180
+)
+
+var (
+	srv       *http.Server
+	baseURL   string
+	serverErr error
+)
+
+func mapToJSONReader(m map[string]interface{}) (io.Reader, error) {
+	bs, err := json.Marshal(m)
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshal map to JSON: %w", err)
+	}
+
+	buf := bytes.NewBuffer(bs)
+	return buf, nil
+}
+
+func TestMain(m *testing.M) {
+	log.Printf("Setting tests up ...")
+
+	initialEnv := os.Getenv("ENVIRONMENT")
+	os.Setenv("ENVIRONMENT", "staging")
+
+	ctx := context.Background()
+
+	srv, baseURL, serverErr = serverutils.StartTestServer(
+		ctx,
+		presentation.PrepareServer,
+		presentation.AllowedOrigins,
+	)
+	if serverErr != nil {
+		log.Printf("unable to start test server: %s", serverErr)
+	}
+
+	pg, err := gorm.NewPGInstance()
+	if err != nil {
+		log.Printf("can't instantiate test repository: %v", err)
+	}
+
+	_, err = initializeTestService(ctx)
+	if err != nil {
+		log.Printf("Error initializing test service: %v", err)
+	}
+
+	// run tests
+	log.Printf("Running tests ...")
+	code := m.Run()
+
+	pg.DB.Migrator().DropTable(&gorm.Facility{})
+	// restore envs
+	os.Setenv(initialEnv, "ENVIRONMENT")
+
+	log.Printf("finished running tests")
+
+	// cleanup here
+	defer func() {
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			log.Printf("test server shutdown error: %s", err)
+		}
+	}()
+
+	os.Exit(code)
+}
+
+// GetGraphQLHeaders gets relevant GraphQLHeaders
+func GetGraphQLHeaders(ctx context.Context) (map[string]string, error) {
+	authorization, err := GetBearerTokenHeader(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("can't Generate Bearer Token: %s", err)
+	}
+	return req.Header{
+		"Accept":        "application/json",
+		"Content-Type":  "application/json",
+		"Authorization": authorization,
+	}, nil
+}
+
+// GetBearerTokenHeader gets bearer Token Header
+func GetBearerTokenHeader(ctx context.Context) (string, error) {
+	TestUserEmail := "test@bewell.co.ke"
+	user, err := firebasetools.GetOrCreateFirebaseUser(ctx, TestUserEmail)
+	if err != nil {
+		return "", fmt.Errorf("can't get or create firebase user: %s", err)
+	}
+
+	if user == nil {
+		return "", fmt.Errorf("nil firebase user")
+	}
+
+	customToken, err := firebasetools.CreateFirebaseCustomToken(ctx, user.UID)
+	if err != nil {
+		return "", fmt.Errorf("can't create custom token: %s", err)
+	}
+
+	if customToken == "" {
+		return "", fmt.Errorf("blank custom token: %s", err)
+	}
+
+	idTokens, err := firebasetools.AuthenticateCustomFirebaseToken(customToken)
+	if err != nil {
+		return "", fmt.Errorf("can't authenticate custom token: %s", err)
+	}
+	if idTokens == nil {
+		return "", fmt.Errorf("nil idTokens")
+	}
+
+	return fmt.Sprintf("Bearer %s", idTokens.IDToken), nil
+}
+
+func initializeTestService(ctx context.Context) (*interactor.Interactor, error) {
+	fc := &firebasetools.FirebaseClient{}
+	_, err := fc.InitFirebase()
+	if err != nil {
+		return nil, err
+	}
+
+	pg, err := gorm.NewPGInstance()
+	if err != nil {
+		return nil, fmt.Errorf("can't instantiate test repository: %v", err)
+	}
+
+	externalExt := externalExtension.NewExternalMethodsImpl()
+
+	db := postgres.NewMyCareHubDb(pg, pg, pg, pg)
+
+	// Initialize facility usecase
+	facilityUseCase := facility.NewFacilityUsecase(db, db, db, db)
+
+	userUsecase := user.NewUseCasesUserImpl(db, db, db, externalExt)
+
+	i := interactor.NewMyCareHubInteractor(facilityUseCase, userUsecase)
+	return i, nil
+}
