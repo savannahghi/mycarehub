8b2aa12ef1fe82e2b103b1c4026d52e55fe84b5d
Salaton <nairouasalaton@gmail.com>
2022-02-18 10:07:56 +0300

    feat: setup getstream library (#239)

---
+++ pkg/mycarehub/infrastructure/services/stream/stream.go
@@ -0,0 +1,44 @@
+package getstream
+
+import (
+	"context"
+	"log"
+	"time"
+
+	stream "github.com/GetStream/stream-chat-go/v5"
+	"github.com/savannahghi/serverutils"
+)
+
+var (
+	getStreamAPIKey          = serverutils.MustGetEnvVar("GET_STREAM_KEY")
+	getStreamAPISecret       = serverutils.MustGetEnvVar("GET_STREAM_SECRET")
+	getStreamTokenExpiryTime = time.Now().UTC().Add(time.Hour * 12)
+)
+
+// ServiceGetStream represents the various Getstream usecases
+type ServiceGetStream interface {
+	CreateGetStreamUserToken(ctx context.Context, userID string) (string, error)
+}
+
+// ChatClient is the service's struct implementation
+type ChatClient struct {
+	client *stream.Client
+}
+
+// NewServiceGetStream initializes a new getstream service
+func NewServiceGetStream() ServiceGetStream {
+	client, err := stream.NewClient(getStreamAPIKey, getStreamAPISecret)
+	if err != nil {
+		log.Fatalf("failed to start getstream client: %v", err)
+	}
+
+	return &ChatClient{
+		client: client,
+	}
+}
+
+// CreateGetStreamUserToken creates a new token for a user with optional expire time. This token is handed
+// to the client side during login. It allows the client side to connect to the chat API for that user.
+func (c *ChatClient) CreateGetStreamUserToken(ctx context.Context, userID string) (string, error) {
+	return c.client.CreateToken(userID, getStreamTokenExpiryTime)
+}
