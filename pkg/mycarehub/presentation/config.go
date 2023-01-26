f5d64d1cc6d131ff56d140d887948c699b5f7f16
Maxwell Githinji <maxwellgithinji@gmail.com>
2023-01-26 12:31:46 +0300

    chore: add depedency injection (#720)
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>
    
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- pkg/mycarehub/presentation/config.go
+++ pkg/mycarehub/presentation/config.go
@@ -11,51 +11,20 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	stream "github.com/GetStream/stream-chat-go/v5"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/kevinburke/twilio-go"
-	"github.com/mailgun/mailgun-go"
 	"github.com/savannahghi/firebasetools"
 	"github.com/savannahghi/interserviceclient"
 	externalExtension "github.com/savannahghi/mycarehub/pkg/mycarehub/application/extension"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/database/postgres"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/database/postgres/gorm"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/services/clinical"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/services/fcm"
-	streamService "github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/services/getstream"
 	loginservice "github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/services/login"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/services/mail"
-	pubsubmessaging "github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/services/pubsub"
-	serviceSMS "github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/services/sms"
-	surveyInstance "github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/services/surveys"
-	serviceTwilio "github.com/savannahghi/mycarehub/pkg/mycarehub/infrastructure/services/twilio"
 	"github.com/savannahghi/mycarehub/pkg/mycarehub/presentation/graph"
 	"github.com/savannahghi/mycarehub/pkg/mycarehub/presentation/graph/generated"
 	internalRest "github.com/savannahghi/mycarehub/pkg/mycarehub/presentation/rest"
 	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases"
-	appointment "github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/appointments"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/authority"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/communities"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/content"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/facility"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/feedback"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/healthdiary"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/metrics"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/notification"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/organisation"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/otp"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/programs"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/questionnaires"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/screeningtools"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/securityquestions"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/servicerequest"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/surveys"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/terms"
-	"github.com/savannahghi/mycarehub/pkg/mycarehub/usecases/user"
 	"github.com/savannahghi/serverutils"
-	"github.com/savannahghi/silcomms"
 	log "github.com/sirupsen/logrus"
+
+	injector "github.com/savannahghi/mycarehub/wire"
 )
 
 const (
@@ -76,22 +45,6 @@ var allowedHeaders = []string{
 	"Content-Type",
 }
 
-var (
-	getStreamAPIKey    = serverutils.MustGetEnvVar("GET_STREAM_KEY")
-	getStreamAPISecret = serverutils.MustGetEnvVar("GET_STREAM_SECRET")
-
-	// surveys
-	surveysBaseURL = serverutils.MustGetEnvVar("SURVEYS_BASE_URL")
-
-	mailGunAPIKey = serverutils.MustGetEnvVar("MAILGUN_API_KEY")
-	mailGunDomain = serverutils.MustGetEnvVar("MAILGUN_DOMAIN")
-
-	twilioAccountSID = serverutils.MustGetEnvVar("TWILIO_ACCOUNT_SID")
-	twilioAuthToken  = serverutils.MustGetEnvVar("TWILIO_ACCOUNT_AUTH_TOKEN")
-
-	clinicalDepsName = "clinical"
-)
-
 // Router sets up the ginContext router
 func Router(ctx context.Context) (*mux.Router, error) {
 	fc := &firebasetools.FirebaseClient{}
@@ -100,102 +53,15 @@ func Router(ctx context.Context) (*mux.Router, error) {
 		return nil, err
 	}
 
-	pg, err := gorm.NewPGInstance()
-	if err != nil {
-		return nil, fmt.Errorf("can't instantiate repository in resolver: %v", err)
-	}
-
 	externalExt := externalExtension.NewExternalMethodsImpl()
 	loginsvc := loginservice.NewServiceLoginImpl(externalExt)
-	db := postgres.NewMyCareHubDb(pg, pg, pg, pg)
 
-	fcmService := fcm.NewService()
-	streamClient, err := stream.NewClient(getStreamAPIKey, getStreamAPISecret)
+	useCases, err := injector.InitializeUseCases(ctx)
 	if err != nil {
-		log.Fatalf("failed to start getstream client: %v", err)
+		return nil, err
 	}
 
-	silCommsLib, err := silcomms.NewSILCommsLib()
-	if err != nil {
-		log.Fatalf("failed to start silcomms client: %v", err)
-	}
-	smsService := serviceSMS.NewServiceSMS(silCommsLib)
-
-	// Twilio
-	httpClient := &http.Client{
-		Timeout: time.Second * twilioHTTPClientTimeoutSeconds,
-	}
-	twilioClient := twilio.NewClient(twilioAccountSID, twilioAuthToken, httpClient)
-	twilioMessageObj := twilioClient.Messages
-	twilioService := serviceTwilio.NewServiceTwilio(twilioMessageObj)
-
-	otpUseCase := otp.NewOTPUseCase(db, db, externalExt, smsService, twilioService)
-
-	getStream := streamService.NewServiceGetStream(streamClient)
-
-	pubSub, err := pubsubmessaging.NewServicePubSubMessaging(externalExt, getStream, db, fcmService)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize pubsub messaging service: %w", err)
-	}
-
-	// Initialize facility usecase
-	facilityUseCase := facility.NewFacilityUsecase(db, db, db, db, pubSub, externalExt)
-
-	// Initialize user usecase
-	notificationUseCase := notification.NewNotificationUseCaseImpl(fcmService, db, db, db, externalExt)
-
-	authorityUseCase := authority.NewUsecaseAuthority(db, db, externalExt, notificationUseCase)
-
-	clinicalClient := externalExtension.NewInterServiceClient(clinicalDepsName, externalExt)
-	clinicalService := clinical.NewServiceClinical(clinicalClient)
-
-	userUsecase := user.NewUseCasesUserImpl(db, db, db, db, externalExt, otpUseCase, authorityUseCase, getStream, pubSub, clinicalService, smsService, twilioService)
-
-	termsUsecase := terms.NewUseCasesTermsOfService(db, db)
-
-	securityQuestionsUsecase := securityquestions.NewSecurityQuestionsUsecase(db, db, db, externalExt)
-
-	contentUseCase := content.NewUseCasesContentImplementation(db, db, externalExt)
-
-	mailClient := mailgun.NewMailgun(mailGunDomain, mailGunAPIKey)
-	mailClient.SetAPIBase(mailgun.ApiBase)
-	mailService := mail.NewServiceMail(mailClient)
-
-	feedbackUsecase := feedback.NewUsecaseFeedback(db, db, mailService)
-
-	serviceRequestUseCase := servicerequest.NewUseCaseServiceRequestImpl(db, db, db, externalExt, userUsecase, notificationUseCase, smsService)
-
-	communitiesUseCase := communities.NewUseCaseCommunitiesImpl(getStream, externalExt, db, db, pubSub, notificationUseCase, db)
-
-	appointmentUsecase := appointment.NewUseCaseAppointmentsImpl(externalExt, db, db, db, pubSub, notificationUseCase)
-
-	healthDiaryUseCase := healthdiary.NewUseCaseHealthDiaryImpl(db, db, db, serviceRequestUseCase)
-
-	screeningToolsUsecases := screeningtools.NewUseCasesScreeningTools(db, db, db, externalExt)
-
-	surveysClient := surveyInstance.ODKClient{
-		BaseURL:    surveysBaseURL,
-		HTTPClient: &http.Client{},
-	}
-	survey := surveyInstance.NewSurveysImpl(surveysClient)
-	surveysUsecase := surveys.NewUsecaseSurveys(survey, db, db, db, notificationUseCase, serviceRequestUseCase, externalExt)
-
-	metricsUsecase := metrics.NewUsecaseMetricsImpl(db)
-	questionnaireUsecase := questionnaires.NewUseCaseQuestionnaire(db, db, db, db, externalExt)
-	programsUsecase := programs.NewUsecasePrograms(db, db, db, externalExt, getStream, pubSub)
-
-	organisationUsecase := organisation.NewUseCaseOrganisationImpl(db, db, db, externalExt, pubSub)
-
-	useCase := usecases.NewMyCareHubUseCase(
-		userUsecase, termsUsecase, facilityUseCase,
-		securityQuestionsUsecase, otpUseCase, contentUseCase, feedbackUsecase, healthDiaryUseCase,
-		serviceRequestUseCase, authorityUseCase, communitiesUseCase, screeningToolsUsecases,
-		appointmentUsecase, notificationUseCase, surveysUsecase, metricsUsecase, questionnaireUsecase,
-		programsUsecase,
-		organisationUsecase,
-	)
-
-	internalHandlers := internalRest.NewMyCareHubHandlersInterfaces(*useCase)
+	internalHandlers := internalRest.NewMyCareHubHandlersInterfaces(*useCases)
 
 	r := mux.NewRouter() // gorilla mux
 	r.Use(
@@ -295,7 +161,7 @@ func Router(ctx context.Context) (*mux.Router, error) {
 		http.MethodDelete,
 	).HandlerFunc(internalHandlers.DeleteUser())
 
-	r.Path("/pubsub").Methods(http.MethodPost).HandlerFunc(pubSub.ReceivePubSubPushMessages)
+	r.Path("/pubsub").Methods(http.MethodPost).HandlerFunc(useCases.Pubsub.ReceivePubSubPushMessages)
 
 	// This endpoint will be used by external services to get a token that will be used to
 	// authenticate against our APIs
@@ -374,7 +240,7 @@ func Router(ctx context.Context) (*mux.Router, error) {
 	authR.Methods(
 		http.MethodPost,
 		http.MethodGet,
-	).HandlerFunc(GQLHandler(ctx, *useCase))
+	).HandlerFunc(GQLHandler(ctx, *useCases))
 
 	return r, nil
 }
